97ab1d92fdbbd8f6ef1ec13dcf610ce6b8a74d89
GeralmyUrena <geralmyuri11@gmail.com>
2022-11-20 19:27:55 -0500

    commit tree cb452bc02e426a98e7a5cd970cd35a10b5d5e0ec

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+ var customerName = 'bob'
+
+ customerName
+
+ function upperCaseCustomerName(){
+      
+      customerName = customerName.toUpperCase()  //nondestructive method made as if destructive 
+ }
+
+ 
+
+ function setBestCustomer(){
+    
+   bestCustomer = "not bob"
+ }
+ 
+ function overwriteBestCustomer(){
+     bestCustomer = "maybe bob"
+ }
+
+ const leastFavoriteCustomer = "juan"
+
+ function changeLeastFavoriteCustomer(){
+
+    leastFavoriteCustomer = "jon"
+
+
+ }
